8ec4a2e94434bfd5233806b8583331de3a915142
xXDemionXx <demion.savrljuga.business@gmail.com>
2026-03-24 18:35:08 +0100

    Moved the includes to dualshock.h

---
+++ dualshock4/dualshock4.c
@@ -0,0 +1,23 @@
+#include "dualshock4.h"
+
+
+
+//
+// Entry Point
+//
+struct uni_platform* get_my_platform(void) {
+    static struct uni_platform plat = {
+        .name = DUALSHOCK4_NAME,
+        .init = my_platform_init,
+        .on_init_complete = my_platform_on_init_complete,
+        .on_device_discovered = my_platform_on_device_discovered,
+        .on_device_connected = my_platform_on_device_connected,
+        .on_device_disconnected = my_platform_on_device_disconnected,
+        .on_device_ready = my_platform_on_device_ready,
+        .on_oob_event = my_platform_on_oob_event,
+        .on_controller_data = my_platform_on_controller_data,
+        .get_property = my_platform_get_property,
+    };
+
+    return &plat;
+}
--- dualshock4/dualshock4.h
+++ dualshock4/dualshock4.h
@@ -1,21 +1,34 @@
 // This is the API that you use to use the controller
 
-
 #ifndef DUALSHOCK4_H
 #define DUALSHOCK4_H
 
 // includes
-//#include <uni.h>
+#include <stdlib.h>
+#include <string.h>
+#include <btstack_port_esp32.h>
+#include <btstack_run_loop.h>
+#include <btstack_stdio_esp32.h>
+#include <hci_dump.h>
+#include <hci_dump_embedded_stdout.h>
+#include <uni.h>
+#include "sdkconfig.h" // We need to see CONFIG_BLUEPAD32_PLATFORM_CUSTOM
 //
+
+#ifndef CONFIG_BLUEPAD32_PLATFORM_CUSTOM
+#error "Must use BLUEPAD32_PLATFORM_CUSTOM"
+#endif
+
+// Defines
+#define DUALSHOCK4_NAME "dualshock4" // Name of the controller (prefixes it's messages to the console)
 
 // Types
 
 // Callable functions
 
 ds4_init_e ds4_init(void);
-ds4_connection_error_e ds4_search_and_connect(void);    // Connect to controller, report error
+ds4_connection_error_e ds4_search_and_connect(void); // Connect to controller, report error
 ds4_data_t ds4_recieve_data(void);
 ds4_connection_error_e ds4_disconnect(void);
-
 
 #endif // DUALSHOCK4_H
