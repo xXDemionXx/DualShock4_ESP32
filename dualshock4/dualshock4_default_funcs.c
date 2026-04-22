13f72f558c95daaadc33ede41cd8f147c2560ac6
xXDemionXx <demion.savrljuga.business@gmail.com>
2026-04-22 13:00:26 +0200

    Added auto scan and connect logic to connect and disconnect.

--- dualshock4/dualshock4_default_funcs.c
+++ dualshock4/dualshock4_default_funcs.c
@@ -41,17 +41,9 @@ void ds4_on_init_complete(void)
 {
     logi(DUALSHOCK4_DEFAULT_NAME ": on_init_complete()\n");
 
-    // Safe to call "unsafe" functions since they are called from BT thread
-
-    // Start scanning
-    // uni_bt_start_scanning_and_autoconnect_unsafe();
-    // uni_bt_allow_incoming_connections(true);
-
-    // Start scanning
     uni_bt_start_scanning_and_autoconnect_unsafe();
     uni_bt_allow_incoming_connections(true);
 
-    // Based on runtime condition, you can delete or list the stored BT keys.
     if (1)
         uni_bt_del_keys_unsafe();
     else
@@ -63,7 +55,7 @@ uni_error_t ds4_on_device_discovered(bd_addr_t addr, const char *name, uint16_t 
     // DualShock 4 is a gamepad so filter out anything that isn't a game pad.
     if (((cod & UNI_BT_COD_MINOR_MASK) & UNI_BT_COD_MINOR_GAMEPAD) != UNI_BT_COD_MINOR_GAMEPAD)
     {
-        logi("Ignoring device - Not a DualShock 4 gamepad\n");
+        logi("Ignoring device - Not a gamepad\n");
         return UNI_ERROR_IGNORE_DEVICE;
     }
 
@@ -73,6 +65,9 @@ uni_error_t ds4_on_device_discovered(bd_addr_t addr, const char *name, uint16_t 
 void ds4_on_device_connected(uni_hid_device_t *d)
 {
     set_ds4_connection_status(DS4_CONNECTED);
+
+    uni_bt_stop_scanning_unsafe();
+    uni_bt_allow_incoming_connections(false);
 
 #ifdef CONFIG_DS4_MODE_EVENT
     ds4_resume_buttons_event_handler();
@@ -84,6 +79,9 @@ void ds4_on_device_connected(uni_hid_device_t *d)
 void ds4_on_device_disconnected(uni_hid_device_t *d)
 {
     set_ds4_connection_status(DS4_DISCONNECTED);
+
+    uni_bt_start_scanning_and_autoconnect_unsafe();
+    uni_bt_allow_incoming_connections(true);
 
 #ifdef CONFIG_DS4_MODE_EVENT
     ds4_suspend_buttons_event_handler();
